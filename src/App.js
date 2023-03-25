55daa1b39ad446bdbffe793ef739576550c9152c
Anwesha Mishra <anwesharoses@gmail.com>
2023-03-25 13:53:57 +0530

    added document and made main component

--- src/App.js
+++ src/App.js
@@ -1,124 +1,154 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
-import PropTypes from "prop-types";
-import List from "./components/List";
-import Alert from "./components/Alert";
-import { UilPlus } from "@iconscout/react-unicons";
-import { UilPen } from "@iconscout/react-unicons";
-import { UilTrashAlt } from "@iconscout/react-unicons";
-import Chart from "./components/Chart";
-import { v4 as uuid } from "uuid";
-import LightTeamA from "./components/LightTeamA";
+// import React, { useState, useEffect } from "react";
+// import PropTypes from "prop-types";
+// import List from "./components/List";
+// import Alert from "./components/Alert";
+// import { UilPlus } from "@iconscout/react-unicons";
+// import { UilPen } from "@iconscout/react-unicons";
+// import { UilTrashAlt } from "@iconscout/react-unicons";
+// import Chart from "./components/Chart";
+// import { v4 as uuid } from "uuid";
+// import LightTeamA from "./components/LightTeamA";
+// // import {Document} from "./components/Document"
+import Document from "./components/Document";
 
-const ml5 = window.ml5;
 
-let sentiment;
 
-const getLocalStorage = () => {
-  let list = localStorage.getItem("list");
-  if (list) {
-    return JSON.parse(localStorage.getItem("list"));
-  } else {
-    return [];
-  }
-};
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+
+} from "react-router-dom";
+import Main from "./components/Main";
+
+
+
+
+// const ml5 = window.ml5;
+
+// let sentiment;
+
+// const getLocalStorage = () => {
+//   let list = localStorage.getItem("list");
+//   if (list) {
+//     return JSON.parse(localStorage.getItem("list"));
+//   } else {
+//     return [];
+//   }
+// };
 
 function App(props) {
-  const unique_id = uuid();
-  const small_id = unique_id.slice(0, 3);
-  const [name, setName] = useState("");
-  const [list, setList] = useState(getLocalStorage());
-  const [isEditing, setIsEditing] = useState(false);
-  const [editID, setEditID] = useState(null);
-  const [alert, setAlert] = useState({ show: false, msg: "hello", type: "successs" });
-  let [text, setText] = useState(0);
-  let [score, setScore] = useState(0);
-  let [modelIsReady, setModelIsReady] = useState(false);
+  // const unique_id = uuid();
+  // const small_id = unique_id.slice(0, 3);
+  // const [name, setName] = useState("");
+  // const [list, setList] = useState(getLocalStorage());
+  // const [isEditing, setIsEditing] = useState(false);
+  // const [editID, setEditID] = useState(null);
+  // const [alert, setAlert] = useState({ show: false, msg: "hello", type: "successs" });
+  // let [text, setText] = useState(0);
+  // let [score, setScore] = useState(0);
+  // let [modelIsReady, setModelIsReady] = useState(false);
 
-  const handleChange = (e) => {
-    setName(e.target.value);
-    calculateSentiment();
-  };
 
-  const calculateSentiment = () => {
-    const prediction = sentiment.predict(name);
-    setScore(prediction.score);
-  };
+  // const handleChange = (e) => {
+  //   setName(e.target.value);
+  //   calculateSentiment();
+  // };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  // const calculateSentiment = () => {
+  //   const prediction = sentiment.predict(name);
+  //   setScore(prediction.score);
+  // };
 
-    if (!name) {
-      // display alert
-    } else if (name && isEditing) {
-      setList(
-        list.map((item) => {
-          if (item.id === editID) {
-            return { ...item, title: name };
-          }
-          return item;
-        })
-      );
-      setName("");
-      setIsEditing(false);
-      setEditID(null);
-    } else {
-      // show alert
-      const newItem = {
-        id: small_id,
-        title: name,
-        sentifn: Math.floor(score.toFixed(5) * 1000),
-      };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
 
-      setList([...list, newItem]);
-      setName("");
-      // setText("");
-    }
-  };
-  useEffect(() => {
-    console.log("loading model");
-    sentiment = ml5.sentiment("movieReviews", modelReady);
-  }, []);
+  //   if (!name) {
+  //     // display alert
+  //   } else if (name && isEditing) {
+  //     setList(
+  //       list.map((item) => {
+  //         if (item.id === editID) {
+  //           return { ...item, title: name };
+  //         }
+  //         return item;
+  //       })
+  //     );
+  //     setName("");
+  //     setIsEditing(false);
+  //     setEditID(null);
+  //   } else {
+  //     // show alert
+  //     const newItem = {
+  //       id: small_id,
+  //       title: name,
+  //       sentifn: Math.floor(score.toFixed(5) * 1000),
+  //     };
 
-  function modelReady() {
-    console.log("Model Loaded!");
-    setModelIsReady(true);
-  }
+  //     setList([...list, newItem]);
+  //     setName("");
+  //     // setText("");
+  //   }
+  // };
+  // useEffect(() => {
+  //   console.log("loading model");
+  //   sentiment = ml5.sentiment("movieReviews", modelReady);
+  // }, []);
 
-  const clearList = () => {
-    setList([]);
-  };
-  const removeItem = (id) => {
-    setList(list.filter((item) => item.id !== id));
-  };
+  // function modelReady() {
+  //   console.log("Model Loaded!");
+  //   setModelIsReady(true);
+  // }
 
-  const editItem = (id) => {
-    const specificItem = list.find((item) => item.id === id);
-    setIsEditing(true);
-    setEditID(id);
-    setName(specificItem.title);
-  };
-  const [userData, setUserdata] = useState({
-    labels: list.map((item) => item.id),
-    datasets: [
-      {
-        label: "Frequency",
-        data: list.map((item) => item.sentifn),
-        borderColor: "blue",
-        borderWidth: 1.5,
-        backgroundColor: "yellow",
-      },
-    ],
-  });
+  // const clearList = () => {
+  //   setList([]);
+  // };
+  // const removeItem = (id) => {
+  //   setList(list.filter((item) => item.id !== id));
+  // };
 
-  useEffect(() => {
-    localStorage.setItem("list", JSON.stringify(list));
-  }, [list]);
+  // const editItem = (id) => {
+  //   const specificItem = list.find((item) => item.id === id);
+  //   setIsEditing(true);
+  //   setEditID(id);
+  //   setName(specificItem.title);
+  // };
+  // const [userData, setUserdata] = useState({
+  //   labels: list.map((item) => item.id),
+  //   datasets: [
+  //     {
+  //       label: "Frequency",
+  //       data: list.map((item) => item.sentifn),
+  //       borderColor: "blue",
+  //       borderWidth: 1.5,
+  //       backgroundColor: "yellow",
+  //     },
+  //   ],
+  // });
 
-  useEffect(() => {}, []);
+  // useEffect(() => {
+  //   localStorage.setItem("list", JSON.stringify(list));
+  // }, [list]);
+
+  // useEffect(() => {}, []);
+
+
+
 
   return (
-    <div>
+    <>
+    <Router >
+      <div className="containe">
+      <Routes>
+        
+        <Route path="/" element={<Main/>}/ >
+        <Route path="/document" element={<Document/>}/ >
+      </Routes>
+      </div>
+    </Router>
+
+    {/* <div>
       <section className="text-gray-600 body-font relative">
         <div className="container px-5 py-8 mx-auto">
           <div className="flex flex-col text-center w-full mb-12">
@@ -162,6 +192,8 @@ function App(props) {
                   >
                     Clear List
                   </button>
+
+
                 </div>
               </div>
             </div>
@@ -174,61 +206,13 @@ function App(props) {
         <Chart chartData={userData} />
       </div>
       {/* <LightTeamA /> */}
-    </div>
+    {/* </div> */} 
+    </>
   );
 }
 
 export default App;
 
-// //https://joelmasters.medium.com/build-an-online-sentiment-analysis-tool-with-ml5-js-and-react-in-10-minutes-83ce0758ee73
-// import "./App.css";
-// import { useState, useEffect } from "react";
-// import "./App.css";
 
-// const ml5 = window.ml5;
 
-// let sentiment;
 
-// function App() {
-//   let [text, setText] = useState("");
-//   let [score, setScore] = useState(0);
-//   let [modelIsReady, setModelIsReady] = useState(false);
-
-//   const handleChange = (e) => {
-//     setText(e.target.value);
-//   };
-
-//   const calculateSentiment = () => {
-//     const prediction = sentiment.predict(text);
-//     setScore(prediction.score);
-//   };
-
-//   useEffect(() => {
-//     console.log("loading model");
-//     sentiment = ml5.sentiment("movieReviews", modelReady);
-//   }, []);
-
-//   function modelReady() {
-//     console.log("Model Loaded!");
-//     setModelIsReady(true);
-//   }
-
-//   return (
-//     <div className="App">
-//       <h1>Sentiment Analyzer</h1>
-//       <textarea
-//         id="input"
-//         onChange={handleChange}
-//         placeholder="hello I like you!"
-//         disabled={!modelIsReady}
-//       ></textarea>
-//       <br />
-//       <p>{modelIsReady ? "" : "Loading model..."}</p>
-//       <button onClick={calculateSentiment}>Calculate</button>
-//       <br />
-//       <p>Sentiment Score: {Math.floor(score.toFixed(5) * 100)}</p>
-//     </div>
-//   );
-// }
-
-// export default App;
---
+++ src/components/Document.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./document.css"
+const Document = () => {
+  return (
+    <>
+    <div className="container">
+        <h1 className="headline">Model Risk Management Document</h1>
+            
+        
+    </div>
+    </>
+  )
+    
+
+}
+
+export default Document
